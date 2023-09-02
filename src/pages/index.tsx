fcc08e81c89807993e4950fddde8c03354a01981
Najmiddin Nazirov <nnazirov@transoxiania.com>
2023-09-02 17:45:47 +0500

    styles of text, button and tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Heading } from '../components';
+import { useState } from 'react';
+import { Button, Heading, Tag, Text } from '../components';
 
 const Index = () => {
+	const [isClick, setIsClick] = useState(false);
+
 	return (
 		<div>
-			<Heading tag='h2'>children</Heading>
+			<Heading tag='h2'>Heading</Heading>
+			<Text size='s'>Text</Text>
+			<Tag size='s' color='red'>
+				Red
+			</Tag>
+			<Tag size='m' color='green'>
+				Green
+			</Tag>
+			<br />
+			<Button appearance='primary'>Primary</Button>
+			<Button appearance='ghost'>Ghost</Button>
+			<Button appearance='ghost' arrow={isClick ? 'down' : 'right'} onClick={() => setIsClick(prev => !prev)}>
+				Arrow
+			</Button>
+			<Button appearance='primary' arrow='down'>
+				Down
+			</Button>
 		</div>
 	);
 };
