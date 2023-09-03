8a4ed5cba2e27d092089a6b4ab724f1ee4e41406
Najmiddin Nazirov <nnazirov@transoxiania.com>
2023-09-03 14:40:15 +0500

    new commit

---
+++ src/page-components/course-page-component/course-page-component.tsx
@@ -0,0 +1,51 @@
+import { CoursePageComponentProps } from './course-page-component.props';
+import styles from './course-page-component.module.css';
+import { Advantages, Heading, HhData, Product, Tag, Text } from '../../components';
+
+const CoursePageComponent = ({ page, products }: CoursePageComponentProps): JSX.Element => {
+	return (
+		<div className={styles.wrapper}>
+			{/* TITLE */}
+			<div className={styles.title}>
+				<Heading tag='h1'>{page.title}</Heading>
+				<div>Sort...</div>
+			</div>
+
+			{/* PRODUCTS */}
+			<div>{products && products.map((c, idx) => <Product key={idx} product={c} />)}</div>
+
+			{/* VACATIONS */}
+			<div className={styles.hhTitle}>
+				<Heading tag='h2'>Vacations - {page.category}</Heading>
+				<Tag color='red' size='m'>
+					hh.uz
+				</Tag>
+			</div>
+
+			{/* HHDATA */}
+			{page.hh && <HhData {...page.hh} />}
+
+			{/* ADVANTAGES */}
+			{page.advantages && page.advantages.length && (
+				<>
+					<Heading tag='h2'>Advantages</Heading>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+
+			{/* DESCRIPTION */}
+			<Text>{page.description}</Text>
+
+			{/* SKILLS */}
+			<Heading tag='h2'>Skills</Heading>
+			{page.tags.length &&
+				page.tags.map(t => (
+					<Tag color='primary' key={t}>
+						{t}
+					</Tag>
+				))}
+		</div>
+	);
+};
+
+export default CoursePageComponent;
