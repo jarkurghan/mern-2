f366dd3ae85dd4556786de9681840bf7a04e0923
Najmiddin Nazirov <nnazirov@transoxiania.com>
2023-09-04 19:32:23 +0500

    form

--- src/components/reivew-form/reivew-form.tsx
+++ src/components/reivew-form/reivew-form.tsx
@@ -3,26 +3,95 @@ import styles from './reivew-form.module.css';
 import cn from 'classnames';
 import Input from '../input/input';
 import Rating from '../rating/rating';
-import { useState } from 'react';
 import { Button, TextArea } from '..';
+import { Controller, useForm } from 'react-hook-form';
+import { IReviewForm, IReviewResponse } from './review-form.interface';
+import axios from 'axios';
+import { useState } from 'react';
+import CloseIcon from './close.svg';
 
 const ReivewForm = ({ productid, className, ...props }: ReviewFormProps): JSX.Element => {
-	const [rating, setRating] = useState<number>(0);
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
+	const [error, setError] = useState<boolean>(false);
+
+	const {
+		register,
+		handleSubmit,
+		control,
+		reset,
+		formState: { errors },
+	} = useForm<IReviewForm>();
+
+	const onSubmit = async (formData: IReviewForm) => {
+		setError(false);
+		setIsSuccess(false);
+		try {
+			const { status } = await axios.post<IReviewResponse>(`${process.env.NEXT_PUBLIC_API}/posts`, {
+				...formData,
+				productId: productid,
+			});
+			if (status === 201) {
+				setIsSuccess(true);
+				reset();
+			}
+		} catch {
+			setError(true);
+		}
+	};
 
 	return (
-		<div className={cn(styles.reviewForm, className)} {...props}>
-			<Input placeholder='Name' className={styles.name} />
-			<Input placeholder='Title' className={styles.title} />
-			<div className={styles.rating}>
-				<span>Rating: </span>
-				<Rating isEditabled rating={rating} setRating={setRating} />
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className={cn(styles.reviewForm, className)} {...props}>
+				<Input
+					placeholder='Name'
+					className={styles.name}
+					error={errors.name}
+					{...register('name', { required: { value: true, message: 'Name is required' } })}
+				/>
+				<Input
+					placeholder='Title'
+					className={styles.title}
+					error={errors.title}
+					{...register('title', { required: { value: true, message: 'Title is required' } })}
+				/>
+				<div className={styles.rating}>
+					<span>Rating: </span>
+					<Controller
+						control={control}
+						name={'rating'}
+						rules={{ required: { value: true, message: 'Rating is required' } }}
+						render={({ field }) => (
+							<Rating isEditabled rating={field.value} error={errors.rating} ref={field.ref} setRating={field.onChange} />
+						)}
+					/>
+				</div>
+				<TextArea
+					placeholder='Description'
+					className={styles.description}
+					error={errors.description}
+					{...register('description', { required: { value: true, message: 'Description is required' } })}
+				/>
+				<div className={styles.submit}>
+					<Button appearance='primary'>Submit</Button>
+					<span className={styles.info}>* Your review will be moderated and reviewed before being published.</span>
+				</div>
 			</div>
-			<TextArea placeholder='Description' className={styles.description} />
-			<div className={styles.submit}>
-				<Button appearance='primary'>Submit</Button>
-				<span className={styles.info}>* Your review will be moderated and reviewed before being published.</span>
-			</div>
-		</div>
+
+			{isSuccess && (
+				<div className={cn(styles.success, styles.panel)}>
+					<div className={styles.successTitle}>Review sent successfully</div>
+					<div>Thanks your review will published after testing</div>
+					<CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+				</div>
+			)}
+
+			{error && (
+				<div className={cn(styles.error, styles.panel)}>
+					<div className={styles.successTitle}>Something wen wrong</div>
+					<CloseIcon className={styles.close} onClick={() => setError(false)} />
+				</div>
+			)}
+		</form>
 	);
 };
 
